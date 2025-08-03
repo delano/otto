c69715fad2895d44fbe21e0dd42cedec8421410d
delano <delano@onetimesecret.com>
2025-08-03 11:26:43 -0700

    Simplify version

--- lib/otto/version.rb
+++ lib/otto/version.rb
@@ -1,29 +1,5 @@
 # lib/otto/version.rb
 
 class Otto
-  # Otto::VERSION
-  #
-  module VERSION
-    def self.to_a
-      load_config
-      version = [@version[:MAJOR], @version[:MINOR], @version[:PATCH]]
-      version << @version[:PRE] unless @version.fetch(:PRE, nil).to_s.empty?
-      version
-    end
-
-    def self.to_s
-      to_a.join('.')
-    end
-
-    def self.inspect
-      to_s
-    end
-
-    def self.load_config
-      return if @version
-
-      require 'yaml'
-      @version = YAML.load_file(File.join(__dir__, '..', '..', 'VERSION.yml'))
-    end
-  end
+  VERSION = '1.2.1'.freeze
 end
